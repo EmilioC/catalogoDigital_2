b96036c4e1f588a2f4aaba68a5fc8e3d923071c7
EmilioC <emicorgul@gmail.com>
2022-02-15 22:56:28 +0100

    Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MaterialGeneralComponent } from './components/material-general/material-general.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'materiales', component: MaterialGeneralComponent },
+  { path: 'buscador', component: BuscadorComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
